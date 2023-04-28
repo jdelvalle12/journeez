664d19279ebe2df894ee3bfe392d496f32389149
Jose Del Valle <jdelvalle88@live.com>
2023-04-27 23:01:20 -0400

    maybe last commit

--- journeez/next.config.js
+++ journeez/next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 
+const path = require('path');
+
 const nextConfig = {
-	reactStrictMode: true,
-	images: {
-	  domains: ['www.weatherbit.io'],
-	},
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  images: {
+    domains: ['www.weatherbit.io'],
+  },
+  // Specify the new path to the "pages" directory
+  // based on the root directory of the project
+  dir: path.join(__dirname, 'src'),
+};
+
+module.exports = nextConfig;
